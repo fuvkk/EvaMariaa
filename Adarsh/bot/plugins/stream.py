8a470bdf75745daa7f049972bf99c05f578f908b
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-07-04 21:48:02 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -96,8 +96,8 @@ async def private_receive_handler(c: Client, m: Message):
             return
     try:
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}{str(log_msg.id)}/?hash={get_hash(log_msg)}"
+        stream_link = f"dotexhub-7psn.onrender.com/watch/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
+        online_link = f"dotexhub-7psn.onrender.com/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
         
         msg_text ="""<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥WATCH  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : LINK WON'T EXPIRE TILL I DELETE</b>"""
 
