8fdc2d7ff1f55630fd8983d77b60e5d4e521f6e2
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-07-04 21:51:33 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -133,8 +133,8 @@ async def channel_receive_handler(bot, broadcast):
         return
     try:
         log_msg = await broadcast.forward(chat_id=Var.BIN_CHANNEL)
-        stream_link = f"{Var.URL}watch/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
-        online_link = f"{Var.URL}{str(log_msg.id)}/?hash={get_hash(log_msg)}"
+        stream_link = f"dotexhub-7psn.onrender.com/watch/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
+        online_link = f"dotexhub-7psn.onrender.com/{str(log_msg.id)}/?hash={get_hash(log_msg)}"
         await log_msg.reply_text(
             text=f"**Channel Name:** `{broadcast.chat.title}`\n**CHANNEL ID:** `{broadcast.chat.id}`\n**Rᴇǫᴜᴇsᴛ ᴜʀʟ:** {stream_link}",
             quote=True
